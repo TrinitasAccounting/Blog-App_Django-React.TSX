e8115b70b561a7bc3d64a11cf18696a06d39168d
Clay Mangum <clay@trinitasaccounting.com>
2025-03-08 17:48:17 -0500

    adding our slugify of the title on our backend, so we have a unique route for each blog

--- new_django_api/blogapp/models.py
+++ new_django_api/blogapp/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 from django.conf import settings
+from django.utils.text import slugify
+from django.utils import timezone
 
 # Create your models here.
 class CustomUser(AbstractUser):
@@ -34,7 +36,7 @@ class Blog(models.Model):
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.SET_NULL, related_name="blogs", null=True)  
     created_at = models.DateTimeField(auto_now_add=True)    #auto_now_add captures the time the blog was first created
     updated_at = models.DateTimeField(auto_now=True)   #auto_now captures the time the blog was updated
-    published_time = models.DateTimeField(blank=True, null=True)
+    published_date = models.DateTimeField(blank=True, null=True)
     is_draft = models.BooleanField(default=True)
     category= models.CharField(max_length=255, choices=CATEGORY, blank=True, null=True)
     featured_image = models.ImageField(upload_to="blog_img", blank=True, null=True)
@@ -42,7 +44,23 @@ class Blog(models.Model):
 
     # This is saying how to order the blogs (not sure if we need this or not, we may can handle this easier in react)
     class Meta:
-        ordering = ["-published_time"]
+        ordering = ["-published_date"]
 
     def __str__(self):
         return self.title
+
+    # This is creating a url from the title (slugifying the title) and then setting it equal to slug value. It is also using a count so if there are titles with the same name it will continue the while loop and incrementing the count until .exists() doesn't exist, then it will have a unique number value I believe
+    def save(self, *args, **kwargs):
+        base_slug = slugify(self.title)
+        slug = base_slug
+        num = 1
+        while Blog.objects.filter(slug=slug).exists():
+            slug = f'{base_slug}-{num}'
+            num += 1
+        self.slug = slug
+
+        # This is setting the published date as soon as the is_draft is set to false
+        if not self.is_draft and self.published_date is None:
+            self.published_date = timezone.now()
+
+        super().save(*args, **kwargs)
